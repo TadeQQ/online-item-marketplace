1bf5ec7ea61da5148fa2d5a59fdcdef99044ae52
TadeQ <tadeuszkiszka1@gmail.com>
2022-12-08 00:01:46 +0100

    Update Navbar.tsx

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -3,13 +3,22 @@ import React from "react";
 import logo from "../../images/logo.png";
 import { Logo } from "./logo/Logo";
 import { NavbarStyled } from "./Navbar.styled";
+import Search from "../Search/Search";
+import { AiOutlineHeart } from "react-icons/ai";
+import { GiUsaFlag } from "react-icons/gi";
 
 const Navbar = () => {
   return (
     <NavbarStyled.Nav>
-      <Logo alt="Item Marketplace" src={logo} height={90} width={350} />
-      <Link href="/Search">Search</Link>
-      <Link href="/Login">Login</Link>
+      <Logo alt="Item Marketplace" src={logo} height={65} width={250} />
+      <Search />
+      <Link href="/Login">Sign in</Link>
+      <button>
+        <GiUsaFlag />
+      </button>
+      <Link href="/Favorites">
+        <AiOutlineHeart />
+      </Link>
     </NavbarStyled.Nav>
   );
 };
