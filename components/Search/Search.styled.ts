fc9ddb0609797ddb5649625211032ac1f6bf7b38
TadeQ <tadeuszkiszka1@gmail.com>
2022-12-08 00:01:41 +0100

    style Search Component

--- components/Search/Search.styled.ts
+++ components/Search/Search.styled.ts
@@ -1,9 +1,22 @@
 import styled from "styled-components";
 
+const Form = styled.form`
+  border: 5px;
+  border-color: black;
+  border-radius: 20px;
+`;
+
 const Button = styled.button`
   color: black;
 `;
 
+const Input = styled.input`
+  border: none;
+  font-size: 20px;
+  font-weight: 2;
+`;
 export const SearchStyled = {
+  Form,
   Button,
+  Input,
 };
