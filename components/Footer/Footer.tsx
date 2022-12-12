d3a7fa82450aa1836e91be1be984f711dddd1c53
TadeQ <tadeuszkiszka1@gmail.com>
2022-12-12 23:55:54 +0100

    Create Footer.tsx

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { FooterStyled } from "./Footer.styled";
+
+const Footer = () => {
+  return (
+    <FooterStyled>
+      <div>© 2022 Online Market by teddy</div>
+      Terms of Use Privacy | Cookies | Interest-based ads | Regions
+    </FooterStyled>
+  );
+};
+
+export default Footer;
