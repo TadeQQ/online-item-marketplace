f8515327f3602f9e63d195d405d0c49e96b2d810
TadeQ <tadeuszkiszka1@gmail.com>
2022-12-16 00:14:20 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import { PageWithLayout } from "../types/PageLayout";
-
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { PageWithLayout } from '../types/PageLayout';
+import { AuthProvider } from '../hooks/useAuth';
 export default function App({ Component, pageProps }: AppProps) {
   const getLayout = (Component as PageWithLayout).getLayout || ((page) => page);
 
-  return getLayout(<Component {...pageProps} />);
+  return <AuthProvider>{getLayout(<Component {...pageProps} />)}</AuthProvider>;
 }
