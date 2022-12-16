f0b70f7619479152a905533567d768ae59296a93
TadeQ <tadeuszkiszka1@gmail.com>
2022-12-16 13:45:54 +0100

    add descripton of app in homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,11 @@ import { List } from '../components/List/List';
 const Home: PageWithLayout = () => {
   return (
     <div>
+      <p>
+        Online Marketplace by Teddy to platforma z ogłoszeniami, na której
+        możesz sprzedawać oraz wyszukiwać ogłoszenia z ofertami sprzedaży.
+        Przeglądaj kategorię i oferty aby znaleźć coś dla siebie!
+      </p>
       <List />
     </div>
   );
