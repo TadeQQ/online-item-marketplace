80d3b91c30c9c412996eb7a562543ed877b231b6
TadeQ <tadeuszkiszka1@gmail.com>
2022-12-21 19:14:46 +0100

    Create SignIn Page

---
+++ pages/SignIn/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { AuthSignInForm } from '../../components/auth/signin/AuthSignInForm';
+const SignInPage = () => {
+  return (
+    <>
+      <AuthSignInForm />
+    </>
+  );
+};
+
+export default SignInPage;
