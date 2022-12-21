4a336906022fce46eab9e2af308c8b482a48fc8c
TadeQ <tadeuszkiszka1@gmail.com>
2022-12-21 19:14:55 +0100

    Create SignUp Page

---
+++ pages/SignUp/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { AuthSignUpForm } from '../../components/auth/signup/AuthSignUpForm';
+const SignUpPage = () => {
+  return (
+    <>
+      <AuthSignUpForm />
+    </>
+  );
+};
+
+export default SignUpPage;
