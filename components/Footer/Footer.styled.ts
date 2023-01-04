1df891d9f5f06f9aa9b551dc90a015cab0e13317
TadeQ <tadeuszkiszka1@gmail.com>
2023-01-04 18:34:49 +0100

    add styling to footer

--- components/Footer/Footer.styled.ts
+++ components/Footer/Footer.styled.ts
@@ -1,5 +1,9 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const FooterStyled = styled.footer`
   font-weight: bold;
+  text-align: center;
+  bottom: 0;
+  background: #c7ecd6;
+  border-top: 1px solid black;
 `;
