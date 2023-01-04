5e82f511771fc9a6002a160fd24cdabdaf947b44
TadeQ <tadeuszkiszka1@gmail.com>
2023-01-04 18:31:17 +0100

    add BaseLayout to UserPage

--- pages/User/index.tsx
+++ pages/User/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import { getBaseLayout } from '../../components/Layout/BaseLayout';
+import { PageWithLayout } from '../../types/PageLayout';
 
-export const UserPage = () => {
+const UserPage: PageWithLayout = () => {
   return <div>my profile name email</div>;
 };
+
+UserPage.getLayout = getBaseLayout;
+export default UserPage;
